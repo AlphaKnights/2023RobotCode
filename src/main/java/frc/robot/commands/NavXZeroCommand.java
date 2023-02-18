f08d8fb6b7601676204a824e1d0c3b05b2cd5c32
Ryan O'Donnell <38996906+rpod24@users.noreply.github.com>
2023-02-17 21:41:22 -0800

    Changed NavXZeroCommand to InstantCommand

--- src/main/java/frc/robot/commands/NavXZeroCommand.java
+++ src/main/java/frc/robot/commands/NavXZeroCommand.java
@@ -5,11 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class NavXZeroCommand extends CommandBase {
+public class NavXZeroCommand extends InstantCommand {
   DriveSubsystem driveSubsystem;
-  boolean executed = false;
   /** Creates a new NavXZero. */
   public NavXZeroCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -18,31 +18,7 @@ public class NavXZeroCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //Zero navx when executed
+  public void initialize() {
     driveSubsystem.zeroHeading();
-    //Set executed true so that the NavX is only zeroed once
-    executed = true;
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    //Once the execute program is called isFinished returns true. 
-    //This makes it so that the command is only run once per button press
-    if (executed) {
-      executed = false;
-      return true;
-    } else {
-      return false;
-    }
   }
 }
